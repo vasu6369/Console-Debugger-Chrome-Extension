0162985e178855294d3e47bf2080dea858c579ca
Vasu.G <149422575+vasu6369@users.noreply.github.com>
2025-03-20 21:49:45 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AIzaSyAGpwwPvKd5eQr59oNKd9MXQKtcPbW5vUw";
+const GEMINI_API_KEY = "YOUR_GEMENI_API_KEY";
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 let errorLogs = [];
 let chatHistory = []; 
